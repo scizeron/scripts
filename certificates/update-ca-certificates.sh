505f6b504ba899b0a8873cca41ad22ff474ef358
stfciz <stephane.cizeron@gmail.com>
2016-06-09 17:13:57 +0200

    add  update-ca-certificates.sh

---
+++ certificates/update-ca-certificates.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+SRC_CA_FOLDER=$1
+SRC_CERT_EXTENSION=*.cer
+EXPECTED_EXTENSION=.crt
+DEST_DIR=/usr/local/share/ca-certificates
+
+cp $SRC_CA_FOLDER/${SRC_CERT_EXTENSION} $DEST_DIR/
+
+for CA_FILE in $DEST_DIR/${SRC_CERT_EXTENSION}
+do
+ openssl x509 -in $CA_FILE -inform der -outform pem -out ${CA_FILE}${EXPECTED_EXTENSION}
+ rm $CA_FILE
+ echo "${CA_FILE}${EXPECTED_EXTENSION} is created"
+done
+
+update-ca-certificates
