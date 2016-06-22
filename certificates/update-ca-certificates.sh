5c12aac8373f0c61eac1bcbc012583abac08eb25
stfciz <stephane.cizeron@gmail.com>
2016-06-22 15:08:03 +0200

    updates

--- certificates/update-ca-certificates.sh
+++ certificates/update-ca-certificates.sh
@@ -18,17 +18,22 @@ if [ $# -eq 0 ]; then
 fi
 
 SRC_CA_FOLDER=$1
-SRC_CERT_EXTENSION=*.cer
 EXPECTED_EXTENSION=.crt
 DEST_DIR=/usr/local/share/ca-certificates
 
-cp $SRC_CA_FOLDER/${SRC_CERT_EXTENSION} $DEST_DIR/
-
-for CA_FILE in $DEST_DIR/${SRC_CERT_EXTENSION}
+for CA_FILE in ${SRC_CA_FOLDER}/
 do
- openssl x509 -in $CA_FILE -inform der -outform pem -out ${CA_FILE}${EXPECTED_EXTENSION}
- rm $CA_FILE
- echo "${CA_FILE}${EXPECTED_EXTENSION} is created"
+ case "$CA_FILE" in
+  *.cer)
+    OUT_FILE=${DEST_DIR}/${CA_FILE}${EXPECTED_EXTENSION}
+    openssl x509 -in $CA_FILE -inform der -outform pem -out ${OUT_FILE}
+    echo "${CA_FILE} has been transformed and copied"
+    ;;
+  *.pem)
+    cp ${CA_FILE} ${DEST_DIR}/${CA_FILE}
+    echo "${CA_FILE} has been copied"
+    ;;
+ esac
 done
 
 update-ca-certificates
